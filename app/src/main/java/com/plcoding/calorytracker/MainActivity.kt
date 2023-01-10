9f8aba3d27a056c04c5fc49a0caddd8cde620a23
William Herlicq <wherlicq@gmail.com>
2023-01-10 10:33:36 +0100

    Implemented gender selection screen.

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import com.whbnd.core.navigation.Route
+import com.whbnd.onboarding_presentation.gender.GenderScreen
 import com.whbnd.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
 
@@ -45,6 +48,7 @@ class MainActivity : ComponentActivity() {
                     composable(Route.GOAL) {
 
                     }
+
                     composable(Route.TRACKER_OVERVIEW) {
 
                     }
--- core/src/main/java/com/whbnd/core/util/UiEvent.kt
+++ core/src/main/java/com/whbnd/core/util/UiEvent.kt
@@ -1,6 +1,6 @@
 package com.whbnd.core.util
 
 sealed class UiEvent {
-    data class  Navigate(val route: String): UiEvent()
+    data class Navigate(val route: String): UiEvent()
     object NavigateUp: UiEvent()
 }
