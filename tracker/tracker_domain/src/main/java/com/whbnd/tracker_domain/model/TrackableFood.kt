0bc2ebd7fe9f8a28c00729595259f2da715fab40
William Herlicq <wherlicq@gmail.com>
2023-01-11 13:06:48 +0100

    Implemented repository.

---
+++ tracker/tracker_domain/src/main/java/com/whbnd/tracker_domain/model/TrackableFood.kt
@@ -0,0 +1,10 @@
+package com.whbnd.tracker_domain.model
+
+data class TrackableFood(
+    val name: String,
+    val imageUrl: String?,
+    val carbsPer100g: Int,
+    val proteinPer100g: Int,
+    val fatPer100g: Int,
+    val caloriesPer100g: Int
+)
