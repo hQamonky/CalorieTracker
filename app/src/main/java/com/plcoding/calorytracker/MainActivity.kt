27d1ad43c3e4b7cb1fd855dccc7c9075a5449e92
William Herlicq <wherlicq@gmail.com>
2023-01-12 19:00:39 +0100

    Implemented search result list.

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
+import com.whbnd.core.domain.preferences.Preferences
 import com.whbnd.core.navigation.Route
 import com.whbnd.onboarding_presentation.activity.ActivityScreen
 import com.whbnd.onboarding_presentation.age.AgeScreen
@@ -28,13 +29,19 @@ import com.whbnd.onboarding_presentation.welcome.WelcomeScreen
 import com.whbnd.tracker_presentation.search.SearchScreen
 import com.whbnd.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @ExperimentalComposeUiApi
 @ExperimentalCoilApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val shouldShowOnboarding = preferences.loadShouldShowOnboarding()
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
@@ -45,7 +52,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     NavHost(
                         navController = navController,
-                        startDestination = Route.WELCOME
+                        startDestination = if (shouldShowOnboarding) {
+                            Route.WELCOME
+                        } else Route.TRACKER_OVERVIEW
                     ) {
                         composable(Route.WELCOME) {
                             WelcomeScreen(onNavigate = navController::navigate)
--- tracker/tracker_presentation/src/main/java/com/whbnd/tracker_presentation/search/components/SearchTextField.kt
+++ tracker/tracker_presentation/src/main/java/com/whbnd/tracker_presentation/search/components/SearchTextField.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.whbnd.core_ui.LocalSpacing
 import com.whbnd.tracker_presentation.R
